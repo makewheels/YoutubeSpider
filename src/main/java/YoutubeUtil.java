580101bedd7c670a278b27cf94ef243b1f4752cc
spring <spring@qbserver.cn>
2019-05-09 23:05:19 +0800

    init

---
+++ src/main/java/YoutubeUtil.java
@@ -0,0 +1,6 @@
+/**
+ * @date 2019-05-09 23:04
+ */
+public class YoutubeUtil {
+    //https://www.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&maxResults=50&playlistId=PLOrDt87s8A3rgc-wMLze9WG_-k-bblrOe&key=AIzaSyDKA_arHLwnXi3Fs16uxBvttFapA26Fy9A
+}
