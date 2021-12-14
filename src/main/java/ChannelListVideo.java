8fcab14a4ddfe4e57fb16d4f7056fc5428a8b098
miuser <miuser@email.com>
2021-12-14 14:58:22 +0800

    google youtube api

--- src/main/java/ChannelListVideo.java
+++ src/main/java/ChannelListVideo.java
@@ -1,20 +1,16 @@
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.http.InputStreamContent;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.youtube.YouTube;
 import com.google.api.services.youtube.model.*;
-import org.mortbay.util.ajax.JSON;
 
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.util.Collections;
 import java.util.List;
 
-public class YoutubeUtil {
+public class ChannelListVideo {
     // You need to set this value for your code to compile.
     // For example: ... DEVELOPER_KEY = "YOUR ACTUAL KEY";
     private static final String DEVELOPER_KEY = "AIzaSyBxqO6e8l-TIk7s9Iglvf9hzcEw1jMsvws";
@@ -38,7 +34,7 @@ public class YoutubeUtil {
     private static void listPlaylistAndVideo() throws GeneralSecurityException, IOException {
         YouTube youtube = getService();
         YouTube.Playlists.List request = youtube.playlists()
-                .list("snippet");
+                .list(Collections.singletonList("snippet"));
         PlaylistListResponse response = request.setKey(DEVELOPER_KEY)
                 .setChannelId("UCqZQJ4600a9wIfMPbYc60OQ")
                 .setMaxResults(50L)
@@ -51,7 +47,7 @@ public class YoutubeUtil {
             System.out.println("playlistId = " + playlistId);
 
             YouTube.PlaylistItems.List itemResponseRequest = youtube.playlistItems()
-                    .list("snippet");
+                    .list(Collections.singletonList("snippet"));
             PlaylistItemListResponse playlistResponse = itemResponseRequest.setKey(DEVELOPER_KEY)
                     .setMaxResults(50L)
                     .setPlaylistId(playlistId)
@@ -67,21 +63,6 @@ public class YoutubeUtil {
         }
     }
 
-    private static void uploadVideo() throws GeneralSecurityException, IOException {
-        YouTube youtubeService = getService();
-        File file = new File("D:\\BaiduNetdiskDownload\\2021.12.02 曾伟婚礼\\男方家摄影师\\男方家摄影师-P2950099.MOV");
-        InputStreamContent mediaContent =
-                new InputStreamContent("application/octet-stream",
-                        new BufferedInputStream(
-                                new FileInputStream(file)));
-        mediaContent.setLength(file.length());
-
-        YouTube.Videos.Insert request = youtubeService.videos()
-                .insert("snippet,status", new Video(), mediaContent);
-        Video response = request.execute();
-        System.out.println(response);
-    }
-
     public static void main(String[] args)
             throws GeneralSecurityException, IOException {
         System.setProperty("http.proxyHost", "127.0.0.1");
@@ -89,8 +70,7 @@ public class YoutubeUtil {
         System.setProperty("https.proxyHost", "127.0.0.1");
         System.setProperty("https.proxyPort", "7890");
 
-//        listPlaylistAndVideo();
-        uploadVideo();
+        listPlaylistAndVideo();
 
     }
 }
