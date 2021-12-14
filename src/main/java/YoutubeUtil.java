7e2fe055d00fcd13a05d2851f4c72fd5b3e4b24a
miuser <miuser@email.com>
2021-12-14 14:12:28 +0800

    google youtube api

--- src/main/java/YoutubeUtil.java
+++ src/main/java/YoutubeUtil.java
@@ -1,10 +1,15 @@
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.InputStreamContent;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.youtube.YouTube;
 import com.google.api.services.youtube.model.*;
+import org.mortbay.util.ajax.JSON;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.util.List;
@@ -30,13 +35,7 @@ public class YoutubeUtil {
                 .build();
     }
 
-    public static void main(String[] args)
-            throws GeneralSecurityException, IOException {
-        System.setProperty("http.proxyHost", "127.0.0.1");
-        System.setProperty("http.proxyPort", "7890");
-        System.setProperty("https.proxyHost", "127.0.0.1");
-        System.setProperty("https.proxyPort", "7890");
-
+    private static void listPlaylistAndVideo() throws GeneralSecurityException, IOException {
         YouTube youtube = getService();
         YouTube.Playlists.List request = youtube.playlists()
                 .list("snippet");
@@ -50,19 +49,48 @@ public class YoutubeUtil {
         for (Playlist playlist : items) {
             String playlistId = playlist.getId();
             System.out.println("playlistId = " + playlistId);
+
             YouTube.PlaylistItems.List itemResponseRequest = youtube.playlistItems()
                     .list("snippet");
-            PlaylistItemListResponse itemResponse = itemResponseRequest.setKey(DEVELOPER_KEY)
+            PlaylistItemListResponse playlistResponse = itemResponseRequest.setKey(DEVELOPER_KEY)
                     .setMaxResults(50L)
                     .setPlaylistId(playlistId)
                     .execute();
-            List<PlaylistItem> videos = itemResponse.getItems();
+
+            List<PlaylistItem> videos = playlistResponse.getItems();
             for (PlaylistItem video : videos) {
                 PlaylistItemSnippet snippet = video.getSnippet();
                 String videoId = snippet.getResourceId().getVideoId();
                 String title = snippet.getTitle();
-                System.out.println("https://www.youtube.com/watch?v=" + videoId);
+                System.out.println(videoId + " " + title);
             }
         }
+    }
+
+    private static void uploadVideo() throws GeneralSecurityException, IOException {
+        YouTube youtubeService = getService();
+        File file = new File("D:\\BaiduNetdiskDownload\\2021.12.02 曾伟婚礼\\男方家摄影师\\男方家摄影师-P2950099.MOV");
+        InputStreamContent mediaContent =
+                new InputStreamContent("application/octet-stream",
+                        new BufferedInputStream(
+                                new FileInputStream(file)));
+        mediaContent.setLength(file.length());
+
+        YouTube.Videos.Insert request = youtubeService.videos()
+                .insert("snippet,status", new Video(), mediaContent);
+        Video response = request.execute();
+        System.out.println(response);
+    }
+
+    public static void main(String[] args)
+            throws GeneralSecurityException, IOException {
+        System.setProperty("http.proxyHost", "127.0.0.1");
+        System.setProperty("http.proxyPort", "7890");
+        System.setProperty("https.proxyHost", "127.0.0.1");
+        System.setProperty("https.proxyPort", "7890");
+
+//        listPlaylistAndVideo();
+        uploadVideo();
+
     }
 }
